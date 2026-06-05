b5d4ef7477a2872428d7a5ab0fc060f566d33dbd
jonah-mendel <119830199+jonah-mendel@users.noreply.github.com>
2026-06-05 18:57:24 +0000

    Deploying to gh-pages from @ jonah-mendel/jonahmendel.github.io@45243e1ab2db5a63a70f6d307cd0a45042bb9475 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "gradcyl1/jonahmendel.github.io",
+    "data-repo": "jonah-mendel/jonahmendel.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
